c3b5a1cc971ac6ad473f13d352e48870743ff8e3
Sascha <sascha@cbs-service.net>
2018-01-28 20:58:59 +0100

    Move brew and tools to separate file

---
+++ .tools.sh
@@ -0,0 +1,6 @@
+###############################################################################
+# Tools                                                                       #
+###############################################################################
+
+# Automatically change macOS Network locations based on WiFi network
+curl -L https://github.com/eprev/locationchanger/raw/master/locationchanger.sh | bash
