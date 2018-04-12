b4caf8fab7aeb6240042d9aa022892d3dcb1084d
Sascha <sascha@cbs-service.net>
2018-04-12 20:58:29 +0200

    Add vscode settings

--- link_files.sh
+++ link_files.sh
@@ -3,4 +3,6 @@
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s -f ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
 
