60e65f0a850a3b0f982a622232036efd1a6dd467
TribuneX <sascha@cbs-service.net>
2019-07-10 09:04:29 +0200

    Add config for fish shell

---
+++ config.fish
@@ -0,0 +1,57 @@
+set fish_greeting
+
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+set -gx  LC_ALL en_GB.UTF-8 
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
+end
+
+# Java developement
+
+alias j10="jhome  -v 10"
+alias j8="jhome -v 1.8.0_152"
+alias j9="jhome  -v 9.0.4"
+alias j7="jhome -v 1.7.0_181"
+
+function jhome
+    set -g -x JAVA_HOME (/usr/libexec/java_home $argv)
+    echo "JAVA_HOME:" $JAVA_HOME
+    echo "java -version:"
+    java -version
+end
+
+funcsave jhome
+
--- link_files.sh
+++ link_files.sh
@@ -4,5 +4,7 @@ ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s -f ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s -f ~/dotfiles/config.fish ~/.config/fish/config.fish
+
 
 
