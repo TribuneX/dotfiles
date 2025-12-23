90ea0dc2069d86e4840b09206a42457c693d4d3d
TribuneX <TribuneX@users.noreply.github.com>
2025-12-24 00:25:10 +0100

    chore: remove obsolete files

--- link_files.sh
+++ link_files.sh
@@ -4,12 +4,9 @@ echo "Setting up your dotfile links"
 
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -s -f ~/dotfiles/zsh/.zshrc ~/.zshrc
 
 ln -s -f ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s -f ~/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
 
-ln -s -f ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s -f ~/dotfiles/.mackup.cfg ~/.mackup.cfg
-ln -s -f ~/dotfiles/.karabiner.json ~/.config/karabiner/karabiner.json
 
